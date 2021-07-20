4473046420380eda133b2198e1833217d504f32d
Erick-Arango04 <eriar1235@gmail.com>
2021-07-19 21:45:18 -0500

    metodos google y face

--- src/regis_pacientes.js
+++ src/regis_pacientes.js
@@ -80,45 +80,28 @@ class regis_pacientes extends Component {
       }
    }
 
-   //funcion para registrarse con google la funcion te la da firebase tal como esta
    regis_google() {
       var provider = new firebase.auth.GoogleAuthProvider();
       firebase
          .auth()
          .signInWithPopup(provider)
-         .then((result) => {
-            firebase.auth().onAuthStateChanged((user) => {
-               var uid = user.uid;
-               var correo = result.user.email;
-               var nombre = result.user.displayName;
-               db.collection("Pacientes")
-                  .doc(uid)
-                  .set({
-                     Email: correo,
-                     Nombre: nombre,
-                     TipoUsuario: "Paciente",
-                  })
-                  .then((docRef) => {
-                     //console.log("Document written with ID: ", docRef.id);
-                     window.location.href = "/perfil_paciente";
-                  })
-                  .catch((error) => {
-                     console.error("Error adding document: ", error);
-                  });
-            });
-
-            // ...
+         .then((user) => {
+            console.log(user);
          })
          .catch((error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
+            console.log(error);
          });
+   }
+
+   regis_face() {
+      let provider = new firebase.auth.FacebookAuthProvider();
+      firebase
+         .auth()
+         .signInWithPopup(provider)
+         .then((user) => {
+            console.log(user);
+         })
+         .catch((error) => console.log(error));
    }
 
    //htnl para que se muestre la pagina
